653fe2b97a656ecace7fa90fcb6c68ae6d9d8b94
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-06-19 14:24:50 -0300

    Update files

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator")
+city_of_the_user = input("What's the name of the city you grew up in?\n")
+pet_name_of_the_user = input("What's your pet's name?\n")
+generated_band_name = city_of_the_user + " " + pet_name_of_the_user
+print("Your band name could be " + generated_band_name)
