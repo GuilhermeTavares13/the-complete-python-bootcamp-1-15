857684907971c201087957053b1825fdd03fc0ca
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-06-22 22:02:41 -0300

    Update files

--- Day 3/Nesting and Elif/task.py
+++ Day 3/Nesting and Elif/task.py
@@ -3,5 +3,13 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120:
     print("You can ride the rollercoaster")
+
+    age = int(input("What is your age? "))
+    if age >= 18:
+        print("You must pay $12.00")
+    elif 12 <= age < 18:
+      print("You must pay $7.00")
+    else:
+        print("You must pay $5.00")
 else:
     print("Sorry you have to grow taller before you can ride.")
