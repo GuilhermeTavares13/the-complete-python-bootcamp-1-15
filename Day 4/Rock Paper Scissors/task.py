d78360eb075f5524ac11e6428001973fe444b221
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-06-29 22:33:31 -0300

    Update files

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,33 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+
+actions = [rock, paper, scissors]
+player_move = actions[int(input('Your move: rock (1), paper (2), scissors (3):\n'))-1]
+computer_move = random.choice(actions)
+
+print(f'Your move:\n{player_move}')
+print(f'Computer move:\n{computer_move}')
+
+if player_move == rock:
+    if computer_move == rock:
+        print('Draw')
+    elif computer_move == paper:
+        print('Computer wins')
+    else:
+        print('Player wins')
+elif player_move == paper:
+    if computer_move == rock:
+        print('Player wins')
+    elif computer_move == paper:
+        print('Draw')
+    else:
+        print('Computer wins')
+else:
+    if computer_move == rock:
+        print('Computer wins')
+    elif computer_move == paper:
+        print('Player wins')
+    else:
+        print('Draw')
